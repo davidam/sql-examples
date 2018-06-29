f76dae6d738f8a76a850c3798c94823c493d6d61
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-06-29 20:53:44 +0200

    ghtorrent-first-steps.sql: creation

---
+++ ghtorrent-first-steps.sql
@@ -0,0 +1,39 @@
+--- You can make queries to github database from https://cloud.google.com/pricing/free
+--- Give me projects and commits of python projects 
+
+    SELECT	
+        project_id,
+        COUNT(*) AS commits
+      FROM
+        [ghtorrent-bq:ght.commits]
+      WHERE
+        project_id IN (
+        SELECT
+          id
+        FROM
+          [ghtorrent-bq:ght.projects]
+        WHERE
+          language="Python"
+          AND deleted=FALSE
+          AND forked_from IS NULL )
+      GROUP BY
+        project_id
+      ORDER BY
+        commits DESC
+      LIMIT
+        1000000
+
+--- Give me the users with more commits in Python between 2016-06-01 and 2016-07-01
+
+select u.login as login, u.location as location, count(c.id) as num_commits
+from [ghtorrent-bq.ght.project_commits] pc join
+     (SELECT id, author_id FROM [ghtorrent-bq.ght.commits] WHERE
+     date(created_at) between date('2016-06-01')
+                          and date('2016-07-01') )c on pc.commit_id = c.id join
+     (SELECT id
+     FROM [ghtorrent-bq.ght.projects] WHERE language = 'Python') p on p.id = pc.project_id join
+     (SELECT login, location, id
+     FROM [ghtorrent-bq.ght.users]
+     WHERE country_code = 'es') u on c.author_id = u.id,
+group by login, location
+order by num_commits desc;
