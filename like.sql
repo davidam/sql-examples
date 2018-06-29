7ed01613a32832ed3016a70daa10c26e648c4e49
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-06-29 22:11:57 +0200

    like.sql, upper.sql: creation

---
+++ like.sql
@@ -0,0 +1,3 @@
+--- You can make queries to github database from https://bigquery.cloud.google.com/table/ghtorrent-bq:ght.issues?pli=1
+
+select count(*) from [ghtorrent-bq:ght.users] where login like 'david%'
