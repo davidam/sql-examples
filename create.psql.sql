6b4a4a412e2e8e1ff8ff19d647e86d1f06789ff3
David Arroyo Menéndez <davidam@gmail.com>
2021-03-10 22:18:30 +0100

    admin-psql.org, create.psql.sql: creation

---
+++ create.psql.sql
@@ -0,0 +1,19 @@
+
+create table a(
+       a INTEGER NOT NULL
+       );
+
+create table b(
+       b1 INTEGER NOT NULL,
+       b2 INTEGER NOT NULL
+       );
+
+insert into a values (1);
+insert into a values (2);
+insert into a values (3);
+insert into a values (4);
+
+insert into b values (3,4);
+insert into b values (4,4);
+insert into b values (1,5);
+insert into b values (1,6);
